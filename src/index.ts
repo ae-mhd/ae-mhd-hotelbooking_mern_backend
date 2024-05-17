30261eb6091a047275dbb8dc3c74e02a0470cc9d
AbdelhakMHD <ae.mehda@gmail.com>
2024-05-17 18:52:28 +0100

    create register and login functionality

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
-
+import userRoutes from "./routes/users";
+import authRoutes from "./routes/auth";
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
 const app = express();
@@ -10,9 +11,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.get("/api/test", (req: Request, res: Response) => {
-  res.json({ message: "Hello world" });
-});
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(7000, () => {
   console.log("server running on prot 7000");
---
+++ src/routes/users.ts
@@ -0,0 +1,49 @@
+import express, { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import User from "../models/user";
+import { check, validationResult } from "express-validator";
+const router = express.Router();
+
+router.post(
+  "/register",
+  [
+    check("firstName", "First Name is required").isString(),
+    check("lastName", "Last Name is required").isString(),
+    check("email", "Email is required").isEmail(),
+    check("password", "Password with 6 or more characters required").isLength({
+      min: 6,
+    }),
+  ],
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ message: errors.array() });
+    }
+    try {
+      const user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res.status(400).json({ message: "User already exist" });
+      }
+      const newUser = new User(req.body);
+
+      await newUser.save();
+      const token = jwt.sign(
+        { userId: newUser.id },
+        process.env.JWT_SECRET_KEY as string,
+        {
+          expiresIn: "1d",
+        }
+      );
+      res.cookie("auth_token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        maxAge: 86400000, // equale one day
+      });
+      return res.sendStatus(200);
+    } catch (error) {
+      console.log(error);
+      res.status(500).send({ message: "Something went wrong" });
+    }
+  }
+);
+export default router;
